f9577181f34f43851188f21743f5527dab2d4462
Nilesh Tiwari <nileshtiwari204203@gmail.com>
2023-03-08 23:35:13 +0530

    final commit

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 var convType = document.getElementById("convType");
 var convTypeValue;
 convType.addEventListener("change", typeSwitch);
+document.addEventListener('DOMContentLoaded', function() {
+    var script = document.createElement('script');
+    script.src = 'js/'+'convLength'+'.js';
+    document.head.appendChild(script);
+  });
+  
 
 function typeSwitch() {
     convTypeValue = convType.value;
@@ -9,5 +15,8 @@ function typeSwitch() {
         sectionsToHide[i].style.display = "none";
     }
     document.getElementById(convTypeValue).style.display = "block";
+    var script = document.createElement('script');
+    script.src = 'js/'+convTypeValue+'.js';
+    document.head.appendChild(script);
 }
 
